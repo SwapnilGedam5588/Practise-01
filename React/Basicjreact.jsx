65b16d41250dfff4baeed93b8c9d653bfb07988f
SWAPNIL GEDAM <sgedam@delaplex.com>
2026-05-08 22:29:08 +0530

    .\Basicjreact.jsx once again committed

--- React/Basicjreact.jsx
+++ React/Basicjreact.jsx
@@ -14,10 +14,16 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-          <a href="https://react.dev" target="_blank">
+          
+        </a>
+      </div>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-          <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -32,6 +38,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      
     </>
   )
 }
